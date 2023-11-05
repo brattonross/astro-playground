c6b904b9debe71784763d572259f106de9269a82
Ross Bratton <bratton.ross@gmail.com>
2023-11-05 01:04:52 +0000

    fix: add basic meta tags

--- packages/playground/src/index.ts
+++ packages/playground/src/index.ts
@@ -181,6 +181,8 @@ function baseHTML(children: string) {
 	return `<!doctype html>
 <html lang="en">
 <head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<script is:inline>
 		if (window.location.search.includes("dir=rtl")) {
 			document.documentElement.dir = "rtl";
